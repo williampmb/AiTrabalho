bbfc56e9bc763ad259878905b94dded56f68f1d9
William <William@LOL-VI>
2016-04-18 12:57:53 -0300

    - First Agent that works ( not good performance) - //TODO - set next goal as closer points of unknown location - //TODO - update each location that passes if it is "" and unknown

--- src/ai/worlds/vacuum/William1VacuumAgent.java
+++ src/ai/worlds/vacuum/William1VacuumAgent.java
@@ -7,9 +7,11 @@ package ai.worlds.vacuum;
 
 import ai.worlds.Location;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
@@ -19,108 +21,292 @@ import java.util.Vector;
  */
 public class William1VacuumAgent extends VacuumAgent {
 
-	Set<Model> internalState = new LinkedHashSet<Model>();
-	List<Location> around = new ArrayList<Location>();
-	boolean allClean = false;
-	boolean lookAround = false;
-	int count =0;
+    Location currentGoal;
+    Map<Location, String> internalState = new HashMap<Location, String>();
+    private boolean goalSetted = false;
+    private boolean allClean;
+    private boolean arrivedAtGoal = false;
+    private boolean firstyThenx = false;
+    private Location lastBump;
 
-	@Override
-	public void determineAction() {
-		Location loc = this.body.loc;
-		Vector p = (Vector) percept;
+    @Override
+    public void determineAction() {
 
-		Location forward = loc.forward(body.heading);
+        Vector p = (Vector) percept;
+        Location loc = this.body.loc;
+        Location forward = loc.forward(body.heading);
 
-		if (p.elementAt(1) == "dirt") {
-			
-			Model m = new Model(loc, "clean");
-			internalState.add(m);
-			action = "suck";
-			
-		} else if (p.elementAt(2) == "home") {
-			if(allClean){
-				action = "shut-down";
-			}else{
-				if(!lookAround){
-					boolean inside = false;
-					for(Model m : internalState){
-						if(m.l.x == forward.x && m.l.y == forward.y){
-							inside = true;
-						}
-					}
-					
-					if(!inside){
-						Model newModel = new Model(forward, "unknown");
-						internalState.add(newModel);
-					}else{
-						count++;
-					}
-					
-					if(count == 4){
-						lookAround=true;
-					}
-					action = "turn left";
-					
-				}else{
-					
-					
-				}
-			}
+        if (p.elementAt(0) == "bump") {
+            internalState.put(forward, "bump");
 
-		} else if (p.elementAt(1) == "dump") {
+            arrivedAtGoal = false;
+            goalSetted = false;
+            firstyThenx = true;
+            if(lastBump != null && (lastBump.x == forward.x && lastBump.y == forward.y)){
+                if(firstyThenx){
+                    firstyThenx = false;
+                }else{
+                    firstyThenx = true;
+                }
+            }
+            lastBump = new Location(forward.x, forward.y);
+            
+            action = "turn left";
+            return;
+        }else if(p.elementAt(1)== "dirt"){
+            internalState.put(loc, "clean");
+            action = "suck";
+            return;
+        }
+        
+        if(allClean){
+            action = "shut-off";
+            return;
+        }
 
-			Model m1 = new Model(loc, "dump");
-			internalState.add(m1);
-			action = "turn left";
-		} else {
-			this.body.heading.forward(this.body.heading);
-		}
+        if (internalState.isEmpty()) {
+            internalState.put(loc, "home");
+            arrivedAtGoal = true;
+        }
 
-		// for(Location l : around){
-		// Model m = new Model(l,"unknown");
-		// }
+        if (arrivedAtGoal) {
+            arrivedAtGoal = false;
+            goalSetted = false;
+            if (p.elementAt(1) == "dirt") {
+                //TODO - to analyze if it is home
+                internalState.put(loc, "clean");
+                action = "suck";
 
-		// if (internalState.isEmpty()) {
-		//
-		// Model unknow = new
-		// Model(this.body.heading.forward(this.body.heading), "unknown");
-		// }
-		//
-		// if (p.elementAt(1) == "dirt") {
-		// Model m = new Model(l, "clean");
-		// internalState.add(m);
-		// action = "suck";
-		// } else if (p.elementAt(2) == "home") {
-		// if (internalState.isEmpty()) {
-		// action = "shut-off";
-		//
-		// } else {
-		//
-		// action = "forward";
-		// }
-		//
-		// } else if (p.elementAt(1) == "dump") {
-		//
-		// Model m1 = new Model(l, "dump");
-		// internalState.add(m1);
-		// action = "turn left";
-		// } else {
-		// this.body.heading.forward(this.body.heading);
-		// }
+                return;
 
-	}
+            } else {
+                internalState.put(loc, "clean");
+                action = "turn left";
+            }
+        }
 
-	class Model {
+        Location around1 = new Location(loc.x, loc.y);
+        around1.x += 1;
+        Location around2 = new Location(loc.x, loc.y);
+        around2.y += 1;
+        Location around3 = new Location(loc.x, loc.y);
+        around3.x -= 1;
+        Location around4 = new Location(loc.x, loc.y);
+        around4.y -= 1;
 
-		Location l;
-		String state;
+        if (internalState.get(around1) == null) {
+            internalState.put(around1, "unknown");
+        }
+        if (internalState.get(around2) == null) {
+            internalState.put(around2, "unknown");
+        }
+        if (internalState.get(around3) == null) {
+            internalState.put(around3, "unknown");
+        }
+        if (internalState.get(around4) == null) {
+            internalState.put(around4, "unknown");
+        }
 
-		Model(Location l, String state) {
-			this.l = l;
-			this.state = state;
-		}
+        if (!goalSetted) {
+            for (Location s : internalState.keySet()) {
+                String state = internalState.get(s);
+                if (state.equals("unknown")) {
+                    currentGoal = s;
+                    goalSetted = true;
+                    allClean = false;
+                    break;
+                }
+                allClean = true;
+            }
+        }
 
-	}
+        // Going to the goal
+        if (!arrivedAtGoal) {
+            if (!firstyThenx) {
+                if (loc.x < currentGoal.x) {
+                    if (loc.x >= forward.x) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.x <= currentGoal.x) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.x > currentGoal.x) {
+                    if (loc.x <= forward.x) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.x >= currentGoal.x) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.y < currentGoal.y) {
+                    if (loc.y >= forward.y) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.y <= currentGoal.y) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.y > currentGoal.y) {
+                    if (loc.y < forward.y) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.y >= currentGoal.y) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.x == currentGoal.x && loc.y == currentGoal.y) {
+                    arrivedAtGoal = true;
+                } else {
+                    System.out.println("some problem");
+                }
+            } else {
+                if (loc.y < currentGoal.y) {
+                    if (loc.y >= forward.y) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.y <= currentGoal.y) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.y > currentGoal.y) {
+                    if (loc.y <= forward.y) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.y >= currentGoal.y) {
+                        action = "forward";
+                        return;
+                    }
+                }
+                if (loc.x < currentGoal.x) {
+                    if (loc.x >= forward.x) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.x <= currentGoal.x) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.x > currentGoal.x) {
+                    if (loc.x <= forward.x) {
+                        action = "turn left";
+                        return;
+                    }
+                    if (forward.x >= currentGoal.x) {
+                        action = "forward";
+                        return;
+                    }
+                } else if (loc.x == currentGoal.x && loc.y == currentGoal.y) {
+                    arrivedAtGoal = true;
+                } else {
+                    System.out.println("some problem");
+                }
+            }
+
+        }
+
+        if (arrivedAtGoal) {
+            arrivedAtGoal = false;
+            goalSetted = false;
+            if (p.elementAt(1) == "dirt") {
+                //TODO - to analyze if it is home
+                internalState.put(loc, "clean");
+                action = "suck";
+
+                return;
+
+            } else {
+                internalState.put(loc, "clean");
+                action = "turn left";
+            }
+        }
+
+//        else if (p.elementAt(0) == "bump") {
+//                internalState.put(loc, "bump");
+//                action = "turn left";
+//                return;
+//
+//            }
+//		if (p.elementAt(1) == "dirt") {
+//			
+//			Model m = new Model(loc, "clean");
+//			internalState.add(m);
+//			action = "suck";
+//			
+//		} else if (p.elementAt(2) == "home") {
+//			if(allClean){
+//				action = "shut-down";
+//			}else{
+//				if(!lookAround){
+//					boolean inside = false;
+//					for(Model m : internalState){
+//						if(m.l.x == forward.x && m.l.y == forward.y){
+//							inside = true;
+//						}
+//					}
+//					
+//					if(!inside){
+//						Model newModel = new Model(forward, "unknown");
+//						internalState.add(newModel);
+//					}
+//                                        
+//					count++;
+//                                        
+//					if(count == 4){
+//						lookAround=true;
+//					}
+//					action = "turn left";
+//					
+//				}else{
+//					
+//					
+//				}
+//			}
+//
+//		} else if (p.elementAt(1) == "dump") {
+//
+//			Model m1 = new Model(loc, "dump");
+//			internalState.add(m1);
+//			action = "turn left";
+//		} else {
+//			this.body.heading.forward(this.body.heading);
+//		}
+        // for(Location l : around){
+        // Model m = new Model(l,"unknown");
+        // }
+        // if (internalState.isEmpty()) {
+        //
+        // Model unknow = new
+        // Model(this.body.heading.forward(this.body.heading), "unknown");
+        // }
+        //
+        // if (p.elementAt(1) == "dirt") {
+        // Model m = new Model(l, "clean");
+        // internalState.add(m);
+        // action = "suck";
+        // } else if (p.elementAt(2) == "home") {
+        // if (internalState.isEmpty()) {
+        // action = "shut-off";
+        //
+        // } else {
+        //
+        // action = "forward";
+        // }
+        //
+        // } else if (p.elementAt(1) == "dump") {
+        //
+        // Model m1 = new Model(l, "dump");
+        // internalState.add(m1);
+        // action = "turn left";
+        // } else {
+        // this.body.heading.forward(this.body.heading);
+        // }
+    }
 
 }
